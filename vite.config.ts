07777180b0a564c14602be8bc1b61699aea956b2
sarthakdehankar84-design <sarthakdehankar84@gmail.com>
2026-04-05 13:26:54 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/repo-name/",   // <-- Yahan apne GitHub repo ka name daalo
   server: {
     host: "::",
     port: 8080,
