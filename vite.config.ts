c7ed415258511c7ad94837aa34aa5a5515001bcf
sarthakdehankar84-design <sarthakdehankar84@gmail.com>
2026-04-05 14:08:46 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/StreakForge/",   // 🔹 Your GitHub repo name
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  base: "/StreakForge/",   // 🔹 Repo name
   plugins: [react()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    alias: { "@": path.resolve(__dirname, "./src") },
   },
 });
