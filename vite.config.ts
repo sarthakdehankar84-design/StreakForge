3dbcc60ab92574598f7db220d930230f8c77175c
sarthakdehankar84-design <sarthakdehankar84@gmail.com>
2026-04-05 13:34:12 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/repo-name/",   // <-- Yahan apne GitHub repo ka name daalo
+  base: "/StreakForge/",   // 🔹 GitHub repo name
   server: {
     host: "::",
     port: 8080,
